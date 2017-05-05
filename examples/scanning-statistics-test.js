d277198f8106169f4a4c1f121b802496c1b45fdb
ganesheth <ganeshr@student.ethz.ch>
2017-05-05 20:43:40 +0200

    Minor fixes

--- examples/scanning-statistics-test.js
+++ examples/scanning-statistics-test.js
@@ -1,13 +1,20 @@
 var noble = require('../index');
 
 noble.on('stateChange', function(state) {
+
+  if(process.argv.length != 6){
+    Consol.log("Usage scanning-statistics-test.js <interval> <window> <type 0=passive 1=active> <expectedTags> <toleranceFactor>");
+    return;
+  }
+
   if (state === 'poweredOn') {
     var serviceUUIDs = ["feaa"];// ["0000" + "FEAA" + "00001000800000805F9B34FB"]; // default: [] => all
     var allowDuplicates = false; // default: false
     var interval = process.argv[2];
     var window = process.argv[3];
-    var type = process.argv[4];
+    var type = process.argv[4];    
     expectedTags = process.argv[5];
+    toleranceFactor = process.argv[6];
     console.log("Scanning for 0x" + serviceUUIDs[0] + " using ScanInterval=" + interval + " ScaWindow=" + window + " (cycle=" + (window/interval) + ") Expected tags=" + expectedTags);
 	  N++;
     noble.overrideScanParameters(interval, window, type);
@@ -26,6 +33,7 @@ var avgTotal = 0;
 var expectedTags = 10;
 var outlierCount = 0;
 var avg = 0;
+var toleranceFactor = 5;
 
 
 noble.on('discover', function(peripheral) {
@@ -53,7 +61,8 @@ noble.on('discover', function(peripheral) {
       rejected = true;
       outlierCount++;
     }
-    else {
+    else 
+    {
       avgTotal += dt;
       avg = avgTotal / N;
       N++;
